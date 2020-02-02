ac41034d65bffc0d6c22e7d6f3cd264c33418834
adamliptrot-oc <adam.liptrot@opencastsoftware.com>
2020-02-02 09:55:49 +0000

    Initial

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Add a practitioner',
+  serviceName: 'Report',
 
   // Default port that prototype runs on
   port: '3000',
